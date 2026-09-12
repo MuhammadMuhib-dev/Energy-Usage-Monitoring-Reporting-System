99c5517337afbca6e6e7b7b71708d11a1379cf2c
Muhib <muhammad.muhib.dev@gmail.com>
2026-09-12 19:48:11 +0500

    Add project files.

---
+++ LCP/Forms/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace LCP
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.grpEntry = new System.Windows.Forms.GroupBox();
+            this.lblFuel = new System.Windows.Forms.Label();
+            this.cmbFuel = new System.Windows.Forms.ComboBox();
+            this.lblMonth = new System.Windows.Forms.Label();
+            this.cmbMonth = new System.Windows.Forms.ComboBox();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.nudYear = new System.Windows.Forms.NumericUpDown();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnManageFuels = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.grpEntry.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudYear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpEntry
+            // 
+            this.grpEntry.Controls.Add(this.lblFuel);
+            this.grpEntry.Controls.Add(this.cmbFuel);
+            this.grpEntry.Controls.Add(this.lblMonth);
+            this.grpEntry.Controls.Add(this.cmbMonth);
+            this.grpEntry.Controls.Add(this.lblYear);
+            this.grpEntry.Controls.Add(this.nudYear);
+            this.grpEntry.Controls.Add(this.lblPrice);
+            this.grpEntry.Controls.Add(this.txtPrice);
+            this.grpEntry.Controls.Add(this.btnSave);
+            this.grpEntry.Controls.Add(this.btnUpdate);
+            this.grpEntry.Controls.Add(this.btnClear);
+            this.grpEntry.Controls.Add(this.btnManageFuels);
+            this.grpEntry.Location = new System.Drawing.Point(15, 15);
+            this.grpEntry.Name = "grpEntry";
+            this.grpEntry.Size = new System.Drawing.Size(700, 140);
+            this.grpEntry.TabIndex = 0;
+            this.grpEntry.TabStop = false;
+            this.grpEntry.Text = "Entry";
+            this.grpEntry.Enter += new System.EventHandler(this.grpEntry_Enter);
+            // 
+            // lblFuel
+            // 
+            this.lblFuel.Location = new System.Drawing.Point(15, 30);
+            this.lblFuel.Name = "lblFuel";
+            this.lblFuel.Size = new System.Drawing.Size(60, 20);
+            this.lblFuel.TabIndex = 0;
+            this.lblFuel.Text = "Fuel:";
+            // 
+            // cmbFuel
+            // 
+            this.cmbFuel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFuel.Location = new System.Drawing.Point(15, 50);
+            this.cmbFuel.Name = "cmbFuel";
+            this.cmbFuel.Size = new System.Drawing.Size(160, 28);
+            this.cmbFuel.TabIndex = 1;
+            // 
+            // lblMonth
+            // 
+            this.lblMonth.Location = new System.Drawing.Point(190, 30);
+            this.lblMonth.Name = "lblMonth";
+            this.lblMonth.Size = new System.Drawing.Size(60, 20);
+            this.lblMonth.TabIndex = 2;
+            this.lblMonth.Text = "Month:";
+            // 
+            // cmbMonth
+            // 
+            this.cmbMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMonth.Location = new System.Drawing.Point(190, 50);
+            this.cmbMonth.Name = "cmbMonth";
+            this.cmbMonth.Size = new System.Drawing.Size(140, 28);
+            this.cmbMonth.TabIndex = 3;
+            this.cmbMonth.SelectedIndexChanged += new System.EventHandler(this.cmbMonth_SelectedIndexChanged);
+            // 
+            // lblYear
+            // 
+            this.lblYear.Location = new System.Drawing.Point(345, 30);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(60, 20);
+            this.lblYear.TabIndex = 4;
+            this.lblYear.Text = "Year:";
+            // 
+            // nudYear
+            // 
+            this.nudYear.Location = new System.Drawing.Point(345, 50);
+            this.nudYear.Maximum = new decimal(new int[] {
+            2100,
+            0,
+            0,
+            0});
+            this.nudYear.Minimum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.nudYear.Name = "nudYear";
+            this.nudYear.Size = new System.Drawing.Size(100, 26);
+            this.nudYear.TabIndex = 5;
+            this.nudYear.Value = new decimal(new int[] {
+            2026,
+            0,
+            0,
+            0});
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.Location = new System.Drawing.Point(460, 30);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(100, 20);
+            this.lblPrice.TabIndex = 6;
+            this.lblPrice.Text = "Price per Unit:";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(460, 50);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(120, 26);
+            this.txtPrice.TabIndex = 7;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(15, 90);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 30);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(125, 90);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 30);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(235, 90);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 30);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnManageFuels
+            // 
+            this.btnManageFuels.Location = new System.Drawing.Point(560, 90);
+            this.btnManageFuels.Name = "btnManageFuels";
+            this.btnManageFuels.Size = new System.Drawing.Size(120, 30);
+            this.btnManageFuels.TabIndex = 11;
+            this.btnManageFuels.Text = "Manage Fuels";
+            this.btnManageFuels.UseVisualStyleBackColor = true;
+            this.btnManageFuels.Click += new System.EventHandler(this.btnManageFuels_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 170);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(700, 380);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(732, 573);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.grpEntry);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fuel Prices Entry";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpEntry.ResumeLayout(false);
+            this.grpEntry.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudYear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpEntry;
+        private System.Windows.Forms.Label lblFuel;
+        private System.Windows.Forms.ComboBox cmbFuel;
+        private System.Windows.Forms.Label lblMonth;
+        private System.Windows.Forms.ComboBox cmbMonth;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.NumericUpDown nudYear;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnManageFuels;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ LCP/Forms/Form1.cs
@@ -0,0 +1,223 @@
+﻿ // NuGet: Microsoft.Data.SqlClient
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LCP
+{
+    public partial class Form1 : Form
+    {
+        private readonly string connString =
+            @"Server=.\SQLEXPRESS;Database=LCP;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        private int? selectedPriceId = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadMonths();
+            LoadFuels();
+            LoadPrices();
+            ClearForm();
+        }
+
+        private void LoadMonths()
+        {
+            cmbMonth.Items.Clear();
+            for (int m = 1; m <= 12; m++)
+                cmbMonth.Items.Add(new MonthItem(m, System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(m)));
+            cmbMonth.DisplayMember = "Name";
+        }
+
+        private class MonthItem
+        {
+            public int Number { get; }
+            public string Name { get; }
+            public MonthItem(int number, string name) { Number = number; Name = name; }
+            public override string ToString() => Name;
+        }
+
+        private void LoadFuels()
+        {
+            using (var conn = new SqlConnection(connString))
+            using (var adapter = new SqlDataAdapter("SELECT FuelId, FuelName FROM Fuels ORDER BY FuelName", conn))
+            {
+                var table = new DataTable();
+                adapter.Fill(table);
+                cmbFuel.DataSource = table;
+                cmbFuel.DisplayMember = "FuelName";
+                cmbFuel.ValueMember = "FuelId";
+            }
+        }
+
+        private void LoadPrices()
+        {
+            using (var conn = new SqlConnection(connString))
+            using (var adapter = new SqlDataAdapter(@"
+                SELECT fp.PriceId, f.FuelName, fp.MonthName, fp.MonthNumber, fp.Year, fp.PricePerUnit
+                FROM FuelPrices fp
+                JOIN Fuels f ON f.FuelId = fp.FuelId
+                ORDER BY fp.Year DESC, fp.MonthNumber DESC", conn))
+            {
+                var table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+                if (dataGridView1.Columns["MonthNumber"] != null)
+                    dataGridView1.Columns["MonthNumber"].Visible = false;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInput()) return;
+            var month = (MonthItem)cmbMonth.SelectedItem;
+
+            using (var conn = new SqlConnection(connString))
+            {
+                string query = @"INSERT INTO FuelPrices (FuelId, MonthNumber, MonthName, Year, PricePerUnit)
+                    VALUES (@FuelId, @MonthNumber, @MonthName, @Year, @PricePerUnit)";
+
+                using (var cmd = new SqlCommand(query, conn))
+                {
+                    AddParameters(cmd, month);
+                    conn.Open();
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex) when (ex.Number == 2627 || ex.Number == 2601)
+                    {
+                        MessageBox.Show("A price for this fuel in this month/year already exists. Use Update instead.",
+                            "Duplicate entry", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                }
+            }
+
+            MessageBox.Show("Price saved.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            LoadPrices();
+            ClearForm();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (selectedPriceId == null)
+            {
+                MessageBox.Show("Select a row from the grid first.", "No record selected",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!ValidateInput()) return;
+            var month = (MonthItem)cmbMonth.SelectedItem;
+
+            using (var conn = new SqlConnection(connString))
+            {
+                string query = @"UPDATE FuelPrices SET
+                    FuelId = @FuelId, MonthNumber = @MonthNumber, MonthName = @MonthName,
+                    Year = @Year, PricePerUnit = @PricePerUnit
+                    WHERE PriceId = @PriceId";
+
+                using (var cmd = new SqlCommand(query, conn))
+                {
+                    AddParameters(cmd, month);
+                    cmd.Parameters.AddWithValue("@PriceId", selectedPriceId.Value);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Price updated.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            LoadPrices();
+            ClearForm();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+        private void ClearForm()
+        {
+            if (cmbFuel.Items.Count > 0) cmbFuel.SelectedIndex = 0;
+            if (cmbMonth.Items.Count > 0) cmbMonth.SelectedIndex = DateTime.Now.Month - 1;
+            nudYear.Value = DateTime.Now.Year;
+            txtPrice.Clear();
+            selectedPriceId = null;
+            dataGridView1.ClearSelection();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            var row = dataGridView1.Rows[e.RowIndex];
+            selectedPriceId = Convert.ToInt32(row.Cells["PriceId"].Value);
+
+            string fuelName = row.Cells["FuelName"].Value.ToString();
+            var fuelTable = (DataTable)cmbFuel.DataSource;
+            foreach (DataRow fuelRow in fuelTable.Rows)
+            {
+                if (fuelRow["FuelName"].ToString() == fuelName)
+                {
+                    cmbFuel.SelectedValue = fuelRow["FuelId"];
+                    break;
+                }
+            }
+
+            int monthNum = Convert.ToInt32(row.Cells["MonthNumber"].Value);
+            cmbMonth.SelectedIndex = monthNum - 1;
+
+            nudYear.Value = Convert.ToInt32(row.Cells["Year"].Value);
+            txtPrice.Text = row.Cells["PricePerUnit"].Value.ToString();
+        }
+
+        private void btnManageFuels_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Open your Manage Fuels form here (add/edit Fuels table entries), " +
+                "then call LoadFuels() again when it closes to refresh this dropdown.",
+                "Manage Fuels", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private bool ValidateInput()
+        {
+            if (cmbFuel.SelectedValue == null || cmbMonth.SelectedItem == null ||
+                !decimal.TryParse(txtPrice.Text, out decimal price) || price <= 0)
+            {
+                MessageBox.Show("Select a fuel and month, and enter a valid price greater than 0.",
+                    "Validation error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        private void AddParameters(SqlCommand cmd, MonthItem month)
+        {
+            cmd.Parameters.AddWithValue("@FuelId", cmbFuel.SelectedValue);
+            cmd.Parameters.AddWithValue("@MonthNumber", month.Number);
+            cmd.Parameters.AddWithValue("@MonthName", month.Name);
+            cmd.Parameters.AddWithValue("@Year", (int)nudYear.Value);
+            cmd.Parameters.AddWithValue("@PricePerUnit", decimal.Parse(txtPrice.Text));
+        }
+
+        private void grpEntry_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
